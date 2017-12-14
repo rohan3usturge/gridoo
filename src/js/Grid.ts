fbc43d673ae1d98b268f76c93d143ce8ac6b7d74
Rohan Usturge <rousturg@microsoft.com>
2017-12-14 20:14:55 +0530

    Grid

--- src/js/Grid.ts
+++ src/js/Grid.ts
@@ -1,16 +1,44 @@
 import * as handlebars from "handlebars";
+import * as gridDetailsRow from "../html/grid-details-row.html";
+import * as gridMainRow from "../html/grid-main-row.html";
 import * as gridHtml from "../html/grid.html";
 
 export class Grid {
 
-    private templateFunction: HandlebarsTemplateDelegate;
+    private templateFunctionForGrid: any;
+    private templateFunctionForMainRow: any;
+    private templateFunctionForDetailsRow: any;
 
     constructor() {
-        this.templateFunction = handlebars.compile(gridHtml);
+        this.templateFunctionForGrid = handlebars.compile(gridHtml);
+        this.templateFunctionForMainRow = handlebars.compile(gridMainRow);
+        this.templateFunctionForDetailsRow = handlebars.compile(gridDetailsRow);
     }
 
-    private bindData = (data: string[]): string => {
-        return this.templateFunction(data);
+    private bindData = (data: any[], columns: any[]): string => {
+        const detailsArray: any[] = [];
+        const mainArray: any[] = [];
+        let tBodyContent: string = "";
+        data.forEach((row: any) => {
+            columns.forEach((col: any) => {
+                const columnValue = row[col.id];
+                // if (col.renderHybridCellDefn) {
+                //     columnValue = col.vxGridRederCellCallback();
+                // }
+                mainArray.push({columnValue});
+                detailsArray.push({
+                    actualValue: row[col.id],
+                    columnName: col.columnName,
+                    columnValue,
+                    id: col.id,
+                });
+            });
+            const mainRowStr = this.templateFunctionForMainRow(mainArray);
+            tBodyContent += mainRowStr;
+            const detailRowStr = this.templateFunctionForDetailsRow(detailsArray);
+            tBodyContent += detailRowStr;
+        });
+        this.templateFunctionForGrid({columns, tBodyContent});
+        return tBodyContent;
     }
-
 }
