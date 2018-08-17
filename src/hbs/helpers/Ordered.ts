e423f89b3d522712e7ff59ca4973124516937a04
Rohan Usturge <rousturg@microsoft.com>
2018-08-17 17:48:28 +0530

    aria label fix

--- src/hbs/helpers/Ordered.ts
+++ src/hbs/helpers/Ordered.ts
@@ -4,6 +4,7 @@ import { IColumn } from "../../js/models/IColumn";
 const ordered = (column: IColumn, index: string): string => {
     const parsedInt = parseInt(index, 10) + 3;
     let ariaSortStr;
+    let ariaLabelStr;
     let headerIconMk;
     const ariaHiddenStr = column.hidden ? " aria-hidden=\"true\"" : " aria-hidden=\"false\"";
     const tabIndex = column.sortable && !column.hidden ? 0 : -1;
@@ -11,14 +12,17 @@ const ordered = (column: IColumn, index: string): string => {
         headerIconMk = " <i class=\"gui gui-icon gui-icon-arrow-up\" aria-hidden=\"false\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down gui-hidden\" aria-hidden=\"true\" ></i>";
         ariaSortStr = "aria-sort=\"ascending\"";
+        ariaLabelStr = column.name + " sorted ascending";
     } else if (column.orderBy === GridOrderDirection.Desc) {
         headerIconMk = "<i class=\"gui gui-icon gui-icon-arrow-up gui-hidden\" aria-hidden=\"true\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down\" aria-hidden=\"false\" ></i>";
         ariaSortStr = "aria-sort=\"descending\"";
+        ariaLabelStr = column.name + " sorted descending";
     } else {
         headerIconMk = "<i class=\"gui gui-icon gui-icon-arrow-up gui-hidden\" aria-hidden=\"true\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down gui-hidden\" aria-hidden=\"true\" ></i>";
         ariaSortStr = "aria-sort=\"none\"";
+        ariaLabelStr = column.name + " Press enter to sort in ascending order.";
     }
     return "<th role=\"columnheader\" aria-colindex=\"" + parsedInt
           + "\" tabindex=\"" + tabIndex + "\" class=\"gui cursor-pointer\" id=\"header-"
