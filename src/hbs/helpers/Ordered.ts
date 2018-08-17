7c12e28d926d5e27b11ab3be76a881beb91f3f81
Rohan Usturge <rousturg@microsoft.com>
2018-08-17 17:37:23 +0530

    removing aria label

--- src/hbs/helpers/Ordered.ts
+++ src/hbs/helpers/Ordered.ts
@@ -26,7 +26,6 @@ const ordered = (column: IColumn, index: string): string => {
           + " data-header-id=\"" + column.id + "\" "
           + ariaSortStr
           + ariaHiddenStr
-          + " aria-label=\"" + column.name + "\" "
           + " title=\"" + column.name + "\" "
           + "  >"
           + " <span class=\"gui sub header m-r-5\">" + column.name + "</span>"
