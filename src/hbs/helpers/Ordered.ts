284554c6fc19f6a20640d7b05bbc67111090da11
Rohan Usturge <rousturg@microsoft.com>
2018-11-09 10:16:47 +0530

    order byfix

--- src/hbs/helpers/Ordered.ts
+++ src/hbs/helpers/Ordered.ts
@@ -12,32 +12,28 @@ const ordered = (column: IColumn, index: string): string => {
         headerIconMk = " <i class=\"gui gui-icon gui-icon-arrow-up\" aria-hidden=\"false\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down gui-hidden\" aria-hidden=\"true\" ></i>";
         ariaSortStr = "aria-sort=\"ascending\"";
-        // ariaLabelStr = column.name + " sorted ascending";
+        ariaLabelStr = column.name + " sorted ascending";
     } else if (column.orderBy === GridOrderDirection.Desc) {
         headerIconMk = "<i class=\"gui gui-icon gui-icon-arrow-up gui-hidden\" aria-hidden=\"true\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down\" aria-hidden=\"false\" ></i>";
         ariaSortStr = "aria-sort=\"descending\"";
-        // ariaLabelStr = column.name + " sorted descending";
+        ariaLabelStr = column.name + " sorted descending";
     } else {
         headerIconMk = "<i class=\"gui gui-icon gui-icon-arrow-up gui-hidden\" aria-hidden=\"true\" ></i> " +
                         "<i class=\"gui gui-icon gui-icon-arrow-down gui-hidden\" aria-hidden=\"true\" ></i>";
         ariaSortStr = "aria-sort=\"none\"";
-        ariaLabelStr = " Press space or enter to sort column " + column.name + " button";
+        ariaLabelStr = column.name + " Press space or enter to sort in ascending order button.";
     }
     return "<th role=\"columnheader\" aria-colindex=\"" + parsedInt
-          + "\" class=\"gui cursor-pointer\" id=\"header-"
+          + "\" tabindex=\"" + tabIndex + "\" class=\"gui cursor-pointer\" id=\"header-"
           + column.id  + "\""
-          + " tabindex=\"" + tabIndex + "\" "
-          + " data-header-id=\"" + column.id + "\" +  "
+          + " data-header-id=\"" + column.id + "\" "
           + ariaSortStr
           + ariaHiddenStr
-          + " aria-labelledBy=\"columnLabel-" + column.id + "\""
-          // + " title=\"" + column.name + "\" "
-          + "  >"
-          + " <span "
-          + " id=\"columnLabel-" + column.id + "\" "
           + " aria-label=\"" + ariaLabelStr + "\" "
-          + " role=\"button\" class=\"gui no-css sub header m-r-5\">" + column.name + "</span>"
+          + " title=\"" + column.name + "\" "
+          + "  >"
+          + " <span class=\"gui sub header m-r-5\">" + column.name + "</span>"
           + headerIconMk
           + " </th>";
 };
