b3dc88e4d264a3689b4b5e9e8da1889629d32293
Rohan Usturge <rousturg@microsoft.com>
2018-11-09 11:12:13 +0530

    focus issue reverted

--- src/js/eventHandlers/HeaderClickHandler.ts
+++ src/js/eventHandlers/HeaderClickHandler.ts
@@ -27,8 +27,8 @@ export class HeaderClickHandler<T> implements IEventHandler<T> {
         // Registering JQuery Event Handler if Header is Clicked.
         this.parentElement.on("click", ".table-header th", this.handleHeaderSort);
         this.parentElement.on("keyup", ".table-header th", this.handleHeaderSort);
-        // this.parentElement.on("mousedown" , ".table-header th", this.handleMouseDown);
-        // this.parentElement.on("focusin", ".table-header th", this.handleHeaderFocus);
+        this.parentElement.on("mousedown" , ".table-header th", this.handleMouseDown);
+        this.parentElement.on("focusin", ".table-header th", this.handleHeaderFocus);
     }
     private handleMouseDown = (event: any) => {
         this.wasHeaderClicked = true;
